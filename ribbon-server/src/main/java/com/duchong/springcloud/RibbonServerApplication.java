dd7288b87406bd7bc470b53f6bbb0fb935818e0c
duchong <1427222829@qq.com>
2019-03-31 11:14:04 +0800

    去重复

--- ribbon-server/src/main/java/com/duchong/springcloud/RibbonServerApplication.java
+++ ribbon-server/src/main/java/com/duchong/springcloud/RibbonServerApplication.java
@@ -2,7 +2,6 @@ package com.duchong.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,7 +10,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
 public class RibbonServerApplication
 {
     public static void main( String[] args )
